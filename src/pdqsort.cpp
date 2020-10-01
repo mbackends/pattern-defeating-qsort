911743a2b97881da8ab3b9f4dd5d9d70d4e136b1
jsilvao <silvaortizjavier@gmail.com>
2020-10-01 11:42:12 -0400

    Add insertion sort edge case optimization
    Fixes #2

--- src/pdqsort.cpp
+++ src/pdqsort.cpp
@@ -7,6 +7,42 @@
 #include <utility>
 #include <iterator>
 
+#if __cplusplus >= 201103L
+    #include <cstdint>
+    #include <type_traits>
+    #define PDQSORT_PREFER_MOVE(x) std::move(x)
+#else
+    #define PDQSORT_PREFER_MOVE(x) (x)
+#endif
+
+
+namespace pdqsort_detail {
+    enum {
+        // Partitions below this size are sorted using insertion sort.
+        insertion_sort_threshold = 24,
+
+        // Partitions above this size use Tukey's ninther to select the pivot.
+        ninther_threshold = 128,
+
+        // When we detect an already sorted partition, attempt an insertion sort that allows this
+        // amount of element moves before giving up.
+        partial_insertion_sort_limit = 8,
+
+        // Must be multiple of 8 due to loop unrolling, and < 256 to fit in unsigned char.
+        block_size = 64,
+
+        // Cacheline size, assumes power of two.
+        cacheline_size = 64
+
+    };
+
+#if __cplusplus >= 201103L
+    template<class T> struct is_default_compare : std::false_type { };
+    template<class T> struct is_default_compare<std::less<T>> : std::true_type { };
+    template<class T> struct is_default_compare<std::greater<T>> : std::true_type { };
+#endif
+
+    // Returns floor(log2(n)), assumes n > 0.
     template<class T>
     inline int log2(T n) {
         int log = 0;
@@ -14,6 +50,54 @@
         return log;
     }
 
+    // Sorts [begin, end) using insertion sort with the given comparison function.
+    template<class Iter, class Compare>
+    inline void insertion_sort(Iter begin, Iter end, Compare comp) {
+        typedef typename std::iterator_traits<Iter>::value_type T;
+        if (begin == end) return;
+
+        for (Iter cur = begin + 1; cur != end; ++cur) {
+            Iter sift = cur;
+            Iter sift_1 = cur - 1;
+
+            // Compare first so we can avoid 2 moves for an element already positioned correctly.
+            if (comp(*sift, *sift_1)) {
+                T tmp = PDQSORT_PREFER_MOVE(*sift);
+
+                do { *sift-- = PDQSORT_PREFER_MOVE(*sift_1); }
+                while (sift != begin && comp(tmp, *--sift_1));
+
+                *sift = PDQSORT_PREFER_MOVE(tmp);
+            }
+        }
+    }
+
+    // Sorts [begin, end) using insertion sort with the given comparison function. Assumes
+    // *(begin - 1) is an element smaller than or equal to any element in [begin, end).
+    template<class Iter, class Compare>
+    inline void unguarded_insertion_sort(Iter begin, Iter end, Compare comp) {
+        typedef typename std::iterator_traits<Iter>::value_type T;
+        if (begin == end) return;
+
+        for (Iter cur = begin + 1; cur != end; ++cur) {
+            Iter sift = cur;
+            Iter sift_1 = cur - 1;
+
+            // Compare first so we can avoid 2 moves for an element already positioned correctly.
+            if (comp(*sift, *sift_1)) {
+                T tmp = PDQSORT_PREFER_MOVE(*sift);
+
+                do { *sift-- = PDQSORT_PREFER_MOVE(*sift_1); }
+                while (comp(tmp, *--sift_1));
+
+                *sift = PDQSORT_PREFER_MOVE(tmp);
+            }
+        }
+    }
+
+    // Attempts to use insertion sort on [begin, end). Will return false if more than
+    // partial_insertion_sort_limit elements were moved, and abort sorting. Otherwise it will
+    // successfully sort and return true.
     template<class Iter, class Compare>
     inline bool partial_insertion_sort(Iter begin, Iter end, Compare comp) {
         typedef typename std::iterator_traits<Iter>::value_type T;
@@ -87,6 +171,11 @@
         }
     }
 
+    // Partitions [begin, end) around pivot *begin using comparison function comp. Elements equal
+    // to the pivot are put in the right-hand partition. Returns the position of the pivot after
+    // partitioning and whether the passed sequence already was correctly partitioned. Assumes the
+    // pivot is a median of at least 3 elements and that [begin, end) is at least
+    // insertion_sort_threshold long. Uses branchless partitioning.
     template<class Iter, class Compare>
     inline std::pair<Iter, bool> partition_right_branchless(Iter begin, Iter end, Compare comp) {
         typedef typename std::iterator_traits<Iter>::value_type T;
@@ -96,17 +185,25 @@
         Iter first = begin;
         Iter last = end;
 
+        // Find the first element greater than or equal than the pivot (the median of 3 guarantees
+        // this exists).
         while (comp(*++first, pivot));
 
+        // Find the first element strictly smaller than the pivot. We have to guard this search if
+        // there was no element before *first.
         if (first - 1 == begin) while (first < last && !comp(*--last, pivot));
         else                    while (                !comp(*--last, pivot));
 
+        // If the first pair of elements that should be swapped to partition are the same element,
+        // the passed in sequence already was correctly partitioned.
         bool already_partitioned = first >= last;
         if (!already_partitioned) {
             std::iter_swap(first, last);
             ++first;
         }
 
+        // The following branchless partitioning is derived from "BlockQuicksort: How Branch
+        // Mispredictions don’t affect Quicksort" by Stefan Edelkamp and Armin Weiss.
         unsigned char offsets_l_storage[block_size + cacheline_size];
         unsigned char offsets_r_storage[block_size + cacheline_size];
         unsigned char* offsets_l = align_cacheline(offsets_l_storage);
@@ -115,6 +212,7 @@
         num_l = num_r = start_l = start_r = 0;
         
         while (last - first > 2 * block_size) {
+            // Fill up offset blocks with elements that are on the wrong side.
             if (num_l == 0) {
                 start_l = 0;
                 Iter it = first;
@@ -144,6 +242,7 @@
                 }
             }
 
+            // Swap elements and update block sizes and first/last boundaries.
             int num = std::min(num_l, num_r);
             swap_offsets(first, last, offsets_l + start_l, offsets_r + start_r,
                          num, num_l == num_r);
@@ -156,12 +255,14 @@
         int l_size = 0, r_size = 0;
         int unknown_left = (int)(last - first) - ((num_r || num_l) ? block_size : 0);
         if (num_r) {
+            // Handle leftover block by assigning the unknown elements to the other block.
             l_size = unknown_left;
             r_size = block_size;
         } else if (num_l) {
             l_size = block_size;
             r_size = unknown_left;
         } else {
+            // No leftover block, split the unknown elements in two blocks.
             l_size = unknown_left/2;
             r_size = unknown_left - l_size;
         }
@@ -209,6 +310,11 @@
         return std::make_pair(pivot_pos, already_partitioned);
     }
 
+    // Partitions [begin, end) around pivot *begin using comparison function comp. Elements equal
+    // to the pivot are put in the right-hand partition. Returns the position of the pivot after
+    // partitioning and whether the passed sequence already was correctly partitioned. Assumes the
+    // pivot is a median of at least 3 elements and that [begin, end) is at least
+    // insertion_sort_threshold long.
     template<class Iter, class Compare>
     inline std::pair<Iter, bool> partition_right(Iter begin, Iter end, Compare comp) {
         typedef typename std::iterator_traits<Iter>::value_type T;
@@ -249,6 +355,10 @@
         return std::make_pair(pivot_pos, already_partitioned);
     }
 
+    // Similar function to the one above, except elements equal to the pivot are put to the left of
+    // the pivot and it doesn't check or return if the passed sequence already was partitioned.
+    // Since this is rarely used (the many equal case), and in that case pdqsort already has O(n)
+    // performance, no block quicksort is applied here for simplicity.
     template<class Iter, class Compare>
     inline Iter partition_left(Iter begin, Iter end, Compare comp) {
         typedef typename std::iterator_traits<Iter>::value_type T;
@@ -301,6 +411,11 @@
                 std::iter_swap(begin, begin + s2);
             } else sort3(begin + s2, begin, end - 1, comp);
 
+            // If *(begin - 1) is the end of the right partition of a previous partition operation
+            // there is no element in [begin, end) that is smaller than *(begin - 1). Then if our
+            // pivot compares equal to *(begin - 1) we change strategy, putting equal elements in
+            // the left partition, greater elements in the right partition. We do not have to
+            // recurse on the left partition, since it's sorted (all equal).
             if (!leftmost && !comp(*(begin - 1), *begin)) {
                 begin = partition_left(begin, end, comp) + 1;
                 continue;
@@ -400,3 +515,8 @@ inline void pdqsort_branchless(Iter begin, Iter end) {
     typedef typename std::iterator_traits<Iter>::value_type T;
     pdqsort_branchless(begin, end, std::less<T>());
 }
+
+
+#undef PDQSORT_PREFER_MOVE
+
+#endif
